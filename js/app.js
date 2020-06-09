b068f8114d10e07e3c49efb1ff8d09e4c0e5921f
Brad Nelson <bnelson13@gmail.com>
2020-06-09 14:43:49 -0600

    Removed SVG from Master

--- js/app.js
+++
@@ -1,8 +0,0 @@
-const path = document.querySelector('.path-animate');
-const length = path.getTotalLength();
-
-path.style.strokeDasharray = length;
-path.style.strokeDashoffset = length;
-
-// we can set the speed as well
-path.style.animationDuration = "2s";
